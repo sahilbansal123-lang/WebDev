1357edd01d6dfafc23b0ca1dda9659c42a5e37fd
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-12 11:09:47 +0530

    js

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,5 +1,16 @@
-console.log("Hello sahil");
-console.log("Hello sahil");
+// console.log("Hello sahil");
+// console.log("Hello sahil");
 
 // window.alert("i really love pizza");
 
+//--------------------------------- VARIABLES -------------------------------------------
+
+let age = 22;
+let name = "sahil";
+let student = true;
+
+age = age + 1;
+
+console.log("You are", age);
+console.log("Hello", name);
+console.log("enrolled", student);
