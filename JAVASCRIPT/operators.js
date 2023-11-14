66d688db572e5ca6a0184a606a102a71fe58c8fd
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-14 21:52:29 +0530

    operator

--- JAVASCRIPT/operators.js
+++ JAVASCRIPT/operators.js
@@ -1,3 +1,20 @@
-
 // OPERATORS AND DATATYPES
 
+// NUMBERS
+let num = 3.14;
+let num2 = 10;
+
+console.log(num + num2);
+
+// STRINGS
+let name = "sahil";
+let lastname = " Bansal";
+console.log(name + lastname);
+
+// BOOLEAN
+let isLoggedin = true;
+console.log(isLoggedin);
+
+// NULL
+let lastLoginDate = null;
+console.log(lastLoginDate);
