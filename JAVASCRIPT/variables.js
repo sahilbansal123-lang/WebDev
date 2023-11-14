e138a8e36fdb6c593e8b75c06cd770e05bdf9158
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-14 21:46:39 +0530

    variables

--- JAVASCRIPT/variables.js
+++ JAVASCRIPT/variables.js
@@ -2,13 +2,20 @@ console.log("hi");
  
 
 // VARIABLES 3 TYPES
-    // 1. VAR
-    // 2. LET
-    // 3. CONST
+    // 1. VAR   it a globally defined
+    // 2. LET   it is not global it is local
+    // 3. CONST const variables values doesn't change
 
-var age =  22;
-var num1 = 10;
-var num2 = 10;
+// var age =  22;
+// var num1 = 10;
+// var num2 = 10;
 
-console.log(age);
-console.log(num1 + num2); 
+// console.log(age);
+// console.log(num1 + num2); 
+
+
+// let age = 22;
+// console.log(age);
+
+const a = 10;
+console.log(a);
