8ce255dfc4bde4c1b77ad768ae655b5ff224ea8a
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-14 21:39:00 +0530

    var

--- JAVASCRIPT/variables.js
+++ JAVASCRIPT/variables.js
@@ -1,1 +1,14 @@
 console.log("hi");
+ 
+
+// VARIABLES 3 TYPES
+    // 1. VAR
+    // 2. LET
+    // 3. CONST
+
+var age =  22;
+var num1 = 10;
+var num2 = 10;
+
+console.log(age);
+console.log(num1 + num2); 
