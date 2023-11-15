d96208512673b44e633c39cef30802c913cd2343
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-15 10:51:15 +0530

    d

--- JAVASCRIPT/ConditionalStatements.js
+++ JAVASCRIPT/ConditionalStatements.js
@@ -1,11 +1,16 @@
-const a = 66;
+const a = 6;
 
 if (a >= 18 & a < 60) {
   console.log("Yes you can vote");
-} 
-else if(a >= 60) {
-    console.log("Now you can retire");
-} 
-else {
+} else if (a >= 60) {
+  console.log("Now you can retire");
+} else {
   console.log("No you can't vote");
 }
+
+// If else using ternarry operators
+const age = 11;
+
+age >= 18 ? console.log("yes") : console.log("No");
+
+
