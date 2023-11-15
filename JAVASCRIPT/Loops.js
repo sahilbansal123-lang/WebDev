ea3714f1bef4c520bef9dc2067c87f9d1ec00824
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-15 12:09:31 +0530

    loops

---
+++ JAVASCRIPT/Loops.js
@@ -0,0 +1,34 @@
+// 3 TYPES OF LOOPS
+//      FOR
+//      WHILE
+//      DO WHILE
+
+// FOR LOOP
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i, "-> Number is divisible by 2");
+//   } else if (!(i % 2 == 0)) {
+//     console.log(i, "Number is not divisible by 2");
+//   }
+// }
+
+// While LOOP
+
+let num = 1;
+// while (num <= 10) {
+//   if (num % 2 == 0) {
+//     console.log(num, "-> Number is divisible by 2");
+//   } else if (!(num % 2 == 0)) {
+//     console.log(num, "Number is not divisible by 2");
+//   }
+//   num++;
+// }
+
+//  DO WHILE LOOP
+
+do {
+  if (num <= 1) {
+    console.log("test");
+  }
+} while (num <= 10);
