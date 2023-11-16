1696f2a0a34e5593eeefdd99c6b6d9bef30fd27e
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-16 12:06:50 +0530

    function

--- JAVASCRIPT/functions.js
+++ JAVASCRIPT/functions.js
@@ -1,7 +1,34 @@
 //      FUNCTIONS
 
-function sayHello() {
-  console.log("Hey Sahil");
-}
+// function sayHello() {
+//   console.log("Hey Sahil");
+// }
 
-sayHello();
+// sayHello();
+
+// function Add(a, b) {
+//   let c = a + b;
+//   return c;
+// }
+
+// function Multiply(a, b) {
+//   let c = a * b;
+//   return c;
+// }
+
+// let add = Add(10, 30);
+// console.log("Hey user the ans is", add);
+
+// let mul = Multiply(10, 30);
+// console.log("Hey user the ans is", mul);
+
+function addNumbers() {
+  let ans = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    ans = ans + arguments[i];
+  }
+  return ans;
+};
+
+let result = addNumbers(1, 2, 3, 4, 5, 6, 7, 8, 9);
+console.log(result);
