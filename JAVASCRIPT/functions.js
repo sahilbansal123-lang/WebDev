b68fa00cb9f4912b44663ead902cc3e6f23e4fcd
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-16 12:18:16 +0530

    function

--- JAVASCRIPT/functions.js
+++ JAVASCRIPT/functions.js
@@ -22,13 +22,26 @@
 // let mul = Multiply(10, 30);
 // console.log("Hey user the ans is", mul);
 
+
+// Function using Argument keyword for taking multiple params
+
 function addNumbers() {
   let ans = 0;
   for (let i = 0; i < arguments.length; i++) {
     ans = ans + arguments[i];
   }
   return ans;
-};
+}
+
+// Function using spread operator usd to take multiple params
+
+function addNumbers(...numbers) {
+  let ans = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    ans = ans + numbers[i];
+  }
+  return ans;
+}
 
 let result = addNumbers(1, 2, 3, 4, 5, 6, 7, 8, 9);
 console.log(result);
