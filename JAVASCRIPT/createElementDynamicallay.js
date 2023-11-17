959f85b7c539923583c2f2b2c7e29b683fb51ce4
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-17 21:53:30 +0530

    dom

--- JAVASCRIPT/createElementDynamicallay.js
+++ JAVASCRIPT/createElementDynamicallay.js
@@ -1,8 +1,16 @@
 const button = document.getElementById("clickButton");
 const container = document.getElementById("my-container");
 
+// button.addEventListener("click", () => {
+//   const el = document.createElement("h1");
+//   el.innerText = "Sahil Bansal";
+//   container.appendChild(el);
+// });
+
+let count = 1;
 button.addEventListener("click", () => {
-  const el = document.createElement("h1");
-  el.innerText = "Sahil Bansal";
+  const el = document.createElement("li");
+  el.innerText = count;
   container.appendChild(el);
+  count++;
 });
