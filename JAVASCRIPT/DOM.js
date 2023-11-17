b855b4a20b5c739a905148b2e053c914731d560e
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-17 11:02:50 +0530

    Query Selector using #id

--- JAVASCRIPT/DOM.js
+++ JAVASCRIPT/DOM.js
@@ -1,7 +1,13 @@
 //  Interaction of javascript with browser
 // DOM == DOCUMENT OBJECT MODEL
 
-// Query Selector
-const body = document.querySelector('body');
-//  = '<h1> HACKED!! </h1>';
-console.log(body.innerHTML)
+// Query Selector using tag name
+// const div = document.querySelector("div");
+// //  = '<h1> HACKED!! </h1>';
+// console.log(div.innerHTML);
+
+
+//  Query Selector using #id
+const username = document.querySelector('#username');
+console.log(username.innerText);
+username.innerText = "John Doe";
