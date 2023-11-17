1418e15da42dc6ea72cd80d60e35a4cbd515474e
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-17 10:54:30 +0530

    queryselector

--- JAVASCRIPT/DOM.js
+++ JAVASCRIPT/DOM.js
@@ -1,3 +1,7 @@
 //  Interaction of javascript with browser
 // DOM == DOCUMENT OBJECT MODEL
 
+// Query Selector
+const body = document.querySelector('body');
+//  = '<h1> HACKED!! </h1>';
+console.log(body.innerHTML)
