6ee4ad7654f32f97f4447916c3f4f2b0703ce392
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-19 10:58:45 +0530

    curring

--- JAVASCRIPT/Closures.js
+++ JAVASCRIPT/Closures.js
@@ -14,9 +14,29 @@ function adder(num) {
   }
   return add;
 }
+// const addto6 = adder(6);
+// const addto10 = adder(10);
 
-const addto6 = adder(6);
-addto6(4);
-addto6(3);
-addto6(2);
-addto6(55);
+// addto6(4);
+// addto6(55);
+
+// addto10(10);
+// addto10(33);
+
+const myName = document.getElementById("my-name");
+const btn = document.getElementById("myButton");
+
+function makeTextSizer(size) {
+  function changeSize() {
+    myName.style.fontSize = `${size}px`;
+  }
+
+  return changeSize;
+}
+const size12 = makeTextSizer(12);
+const size20 = makeTextSizer(20);
+const size5 = makeTextSizer(5);
+const size44 = makeTextSizer(44);
+const size70 = makeTextSizer(70);
+
+btn.addEventListener("click", size70);
