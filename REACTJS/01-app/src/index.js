aaa167ffc75888ecbf1c949f443461c95eabdc77
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-20 10:54:43 +0530

    jsx

--- REACTJS/01-app/src/index.js
+++ REACTJS/01-app/src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
-// // Component 
+// // Component
 // function HelloWorld() {
 //   return <h1> Hello World</h1>;
 // }
 
+// JSX
+function MyComponent() {
+  const name = "Sahil Bansal";
+  return <h2> Hello {name} </h2>;
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HelloWorld />);
-
-
+root.render(<MyComponent />);
