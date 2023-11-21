0bd6cb0206cfff67d00cc3ced31b6ba1e590ff15
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-21 09:06:45 +0530

    header

--- REACTJS/To-Do/todo/src/App.js
+++ REACTJS/To-Do/todo/src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
-
+import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
-    return<p>TODOIE APP</p>
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
