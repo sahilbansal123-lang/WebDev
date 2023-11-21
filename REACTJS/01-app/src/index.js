95c075748450455db02e2f3646b54d54ddea4fdc
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-21 08:55:47 +0530

    jsx

--- REACTJS/01-app/src/index.js
+++ REACTJS/01-app/src/index.js
@@ -9,8 +9,17 @@ import ReactDOM from "react-dom/client";
 // JSX
 function MyComponent() {
   const name = "Sahil Bansal";
-  return <h2> Hello {name} </h2>;
+  return <h2> Hello {userName()} </h2>;
 }
+
+function userName() {
+  return user.firstName + " " + user.lastName;
+}
+
+let user = {
+  firstName: "sahil",
+  lastName: "Bansal",
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<MyComponent />);
