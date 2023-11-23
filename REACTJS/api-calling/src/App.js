8f246937c6123c53e6d98bc2e4e65498334be7f3
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-23 10:42:48 +0530

    api calling

--- REACTJS/api-calling/src/App.js
+++ REACTJS/api-calling/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { getPosts } from "./Api";
+import { useEffect, useState } from "react";
+
+import PostCard from "./components/PostCard";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    getPosts().then((posts) => setData(posts));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data ? (
+        data.map((e) => <PostCard title={e.title} body={e.body} />)
+      ) : (
+        <p>No Data</p>
+      )}
     </div>
   );
 }
