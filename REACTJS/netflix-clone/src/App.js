ff4528aac74c40ecd54f6c0f28f53dd928400e2a
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-25 12:39:30 +0530

    props

--- REACTJS/netflix-clone/src/App.js
+++ REACTJS/netflix-clone/src/App.js
@@ -2,7 +2,13 @@ import React from "react";
 import Cards from "./Cards";
 
 const App = () => {
-  return <Cards />;
+  return (
+    <>
+      <Cards movieName="DARK" />
+      <Cards movieName="13 reaons why" />
+      <Cards movieName="SELL DRUGS ONLINE" />
+    </>
+  );
 };
 
 export default App;
--- REACTJS/netflix-clone/src/Cards.jsx
+++ REACTJS/netflix-clone/src/Cards.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Cards = () => {
+const Cards = (props) => {
   return (
     <div className="cards">
       <div className="card">
         <img src="" alt="myPic" className="card__img" />
         <div className="card__info">
           <span className="card__category">A Netflix original Series</span>
-          <h3 className="card__title">DARK</h3>
+          <h3 className="card__title">{props.movieName}</h3>
           <a href="" target="_blank">
             <button>Watch Now</button>
           </a>
