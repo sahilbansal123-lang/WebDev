ae97c5f3613802f6b12ff2a28a2e0201456b744e
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-25 10:16:57 +0530

    v

--- REACTJS/basic/src/context/Counter.jsx
+++ REACTJS/basic/src/context/Counter.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import { createContext } from "react";
 
 export const CounterContext = createContext(null);
 
 export const CounterProvider = (props) => {
-  return <CounterContext.Provider>{props.children}</CounterContext.Provider>;
+  const [count, setCount] = useState(0);
+  return (
+    <CounterContext.Provider value={{ count }}>
+      {props.children}
+    </CounterContext.Provider>
+  );
 };
+
