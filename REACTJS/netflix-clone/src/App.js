e045a19620e0aca37104ed985089548f9ef8ad35
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-27 12:17:14 +0530

    REACT MAPPING

--- REACTJS/netflix-clone/src/App.js
+++ REACTJS/netflix-clone/src/App.js
@@ -4,40 +4,22 @@ import "./index.css";
 import Sdata from "./SData";
 
 const App = () => {
+  function ncard(val) {
+    return (
+      <Cards
+        movieName={val.sname}
+        imgsrc={val.imgsrc}
+        title={val.title}
+        link={val.links}
+      />
+    );
+  }
+
   return (
     <>
       <h1 className="heading-style">List of top 5 Netflix Series in 2023</h1>
 
-      <Cards
-        movieName={Sdata[0].sname}
-        imgsrc={Sdata[0].imgsrc}
-        title={Sdata[0].title}
-        link={Sdata[0].links}
-      />
-      <Cards
-        movieName={Sdata[1].sname}
-        imgsrc={Sdata[1].imgsrc}
-        title={Sdata[1].title}
-        link={Sdata[1].links}
-      />
-      <Cards
-        movieName={Sdata[2].sname}
-        imgsrc={Sdata[2].imgsrc}
-        title={Sdata[2].title}
-        link={Sdata[2].links}
-      />
-      <Cards
-        movieName={Sdata[3].sname}
-        imgsrc={Sdata[3].imgsrc}
-        title={Sdata[3].title}
-        link={Sdata[3].links}
-      />
-      <Cards
-        movieName={Sdata[4].sname}
-        imgsrc={Sdata[4].imgsrc}
-        title={Sdata[4].title}
-        link={Sdata[4].links}
-      />
+      {Sdata.map(ncard)}
     </>
   );
 };
