bd7aace9dbcca68006001245c7eafe6c56f209e5
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-27 10:55:26 +0530

    reacr

--- REACTJS/netflix-clone/src/App.js
+++ REACTJS/netflix-clone/src/App.js
@@ -1,29 +1,43 @@
 import React from "react";
 import Cards from "./cards";
+import "./index.css";
+import Sdata from "./SData";
 
 const App = () => {
   return (
     <>
+      <h1 className="heading-style">List of top 5 Netflix Series in 2023</h1>
+
       <Cards
-        movieName="DARK"
-        imgsrc="https://resizing.flixster.com/lpJkDxnEFNQT1OWJjnmYfvpAHJ0=/ems.cHJkLWVtcy1hc3NldHMvdHZzZXJpZXMvUlRUVjI2NjgyOS53ZWJw"
-        title="A Netflix Originals "
-        link="https://resizing.flixster.com/lpJkDxnEFNQT1OWJjnmYfvpAHJ0=/ems.cHJkLWVtcy1hc3NldHMvdHZzZXJpZXMvUlRUVjI2NjgyOS53ZWJw"
+        movieName={Sdata[0].sname}
+        imgsrc={Sdata[0].imgsrc}
+        title={Sdata[0].title}
+        link={Sdata[0].links}
       />
       <Cards
-        movieName="DARK"
-        imgsrc="https://resizing.flixster.com/lpJkDxnEFNQT1OWJjnmYfvpAHJ0=/ems.cHJkLWVtcy1hc3NldHMvdHZzZXJpZXMvUlRUVjI2NjgyOS53ZWJw"
-        title="A Netflix Originals "
-        link="https://resizing.flixster.com/lpJkDxnEFNQT1OWJjnmYfvpAHJ0=/ems.cHJkLWVtcy1hc3NldHMvdHZzZXJpZXMvUlRUVjI2NjgyOS53ZWJw"
+        movieName={Sdata[1].sname}
+        imgsrc={Sdata[1].imgsrc}
+        title={Sdata[1].title}
+        link={Sdata[1].links}
       />
       <Cards
-        movieName="DARK"
-        imgsrc="https://resizing.flixster.com/lpJkDxnEFNQT1OWJjnmYfvpAHJ0=/ems.cHJkLWVtcy1hc3NldHMvdHZzZXJpZXMvUlRUVjI2NjgyOS53ZWJw"
-        title="A Netflix Originals "
-        link="https://resizing.flixster.com/lpJkDxnEFNQT1OWJjnmYfvpAHJ0=/ems.cHJkLWVtcy1hc3NldHMvdHZzZXJpZXMvUlRUVjI2NjgyOS53ZWJw"
+        movieName={Sdata[2].sname}
+        imgsrc={Sdata[2].imgsrc}
+        title={Sdata[2].title}
+        link={Sdata[2].links}
       />
-      {/* <Cards movieName="13 reaons why" />
-      <Cards movieName="SELL DRUGS ONLINE" /> */}
+      <Cards
+        movieName={Sdata[3].sname}
+        imgsrc={Sdata[3].imgsrc}
+        title={Sdata[3].title}
+        link={Sdata[3].links}
+      />
+      <Cards
+        movieName={Sdata[4].sname}
+        imgsrc={Sdata[4].imgsrc}
+        title={Sdata[4].title}
+        link={Sdata[4].links}
+      />
     </>
   );
 };
---
+++ REACTJS/netflix-clone/src/SData.jsx
@@ -0,0 +1,41 @@
+const Sdata = [
+  {
+    sname: "DARK",
+    imgsrc:
+      "https://resizing.flixster.com/lpJkDxnEFNQT1OWJjnmYfvpAHJ0=/ems.cHJkLWVtcy1hc3NldHMvdHZzZXJpZXMvUlRUVjI2NjgyOS53ZWJw",
+    title: "Netflix Original series",
+    links:
+      "https://resizing.flixster.com/lpJkDxnEFNQT1OWJjnmYfvpAHJ0=/ems.cHJkLWVtcy1hc3NldHMvdHZzZXJpZXMvUlRUVjI2NjgyOS53ZWJw",
+  },
+  {
+    sname: "LUCIFER",
+    imgsrc:
+      "https://m.media-amazon.com/images/M/MV5BNDJjMzc4NGYtZmFmNS00YWY3LThjMzQtYzJlNGFkZGRiOWI1XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg",
+    title: "Netflix Original series",
+    links:
+      "https://m.media-amazon.com/images/M/MV5BNDJjMzc4NGYtZmFmNS00YWY3LThjMzQtYzJlNGFkZGRiOWI1XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg",
+  },
+  {
+    sname: "MONEY HEIST",
+    imgsrc:
+      "https://static.javatpoint.com/top10-technologies/images/top-10-netflix-series-of-all-time2.png",
+    title: "Netflix Original series",
+    links:
+      "https://static.javatpoint.com/top10-technologies/images/top-10-netflix-series-of-all-time2.png",
+  },
+  {
+    sname: "YOU",
+    imgsrc:
+      "https://i0.wp.com/www.nerdsandbeyond.com/wp-content/uploads/2022/12/Fi522I4VQAAeK9g.jpeg?fit=800%2C1001&ssl=1",
+    title: "Netflix Original series",
+    links:
+      "https://i0.wp.com/www.nerdsandbeyond.com/wp-content/uploads/2022/12/Fi522I4VQAAeK9g.jpeg?fit=800%2C1001&ssl=1",
+  },
+  {
+    sname: "Eternally Confused and Eager for Love",
+    imgsrc: "https://images.indianexpress.com/2022/02/netflix1200.jpg",
+    title: "Netflix Original series",
+    links: "https://images.indianexpress.com/2022/02/netflix1200.jpg",
+  },
+];
+export default Sdata;
