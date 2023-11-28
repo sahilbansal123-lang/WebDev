901bd59865722c96dee138c5d7a7fb21087ed962
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-28 10:53:13 +0530

    time

--- REACTJS/01-app/src/App.jsx
+++ REACTJS/01-app/src/App.jsx
@@ -2,15 +2,23 @@ import React, { useState } from "react";
 import "./index.css";
 
 const App = () => {
-  const [Count, setCount] = useState(0);
-  const IncNum = () => {
-    setCount(Count + 1);
+  // const [Count, setCount] = useState(0);
+  // const IncNum = () => {
+  //   setCount(Count + 1);
+  // };
+
+  let newTime = new Date().toLocaleTimeString();
+  const [Time, setTime] = useState(newTime);
+
+  const time = () => {
+    newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
   };
 
   return (
     <>
-      <h1>{Count}</h1>
-      <button onClick={IncNum}>Click</button>
+      <h1>{Time}</h1>
+      <button onClick={time}>Get Time</button>
     </>
   );
 };
