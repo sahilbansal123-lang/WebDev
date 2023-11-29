7752169b355c8d6e4a3afa41e09e14e9fc5654a4
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-11-29 10:40:51 +0530

    dx

--- REACTJS/01-app/src/App.jsx
+++ REACTJS/01-app/src/App.jsx
@@ -1,24 +1,40 @@
 import React, { useState } from "react";
 import "./index.css";
 
+import Events from "./Components/Events";
+
 const App = () => {
   // const [Count, setCount] = useState(0);
   // const IncNum = () => {
   //   setCount(Count + 1);
   // };
 
-  let newTime = new Date().toLocaleTimeString();
-  const [Time, setTime] = useState(newTime);
+  // let newTime = new Date().toLocaleTimeString();
+  // const [Time, setTime] = useState(newTime);
 
-  const time = () => {
-    newTime = new Date().toLocaleTimeString();
-    setTime(newTime);
-  };
+  // const time = () => {
+  //   newTime = new Date().toLocaleTimeString();
+  //   setTime(newTime);
+  // };
+
+  // let time = new Date().toLocaleTimeString();
+  // const [Time, setTime] = useState(time);
+
+  // const updateTime = () => {
+  //   time = new Date().toLocaleTimeString();
+  //   setTime(time);
+  // };
+
+  // setInterval(updateTime, 1000);
 
   return (
     <>
-      <h1>{Time}</h1>
-      <button onClick={time}>Get Time</button>
+      {/* <h1>{Time}</h1>
+      <button onClick={time}>Get Time</button> */}
+
+      {/* <h1>{Time}</h1> */}
+
+      <Events />
     </>
   );
 };
---
+++ REACTJS/01-app/src/Components/Events.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Events = () => {
+  return (
+    <div >
+      <button className="button">CLICK ME</button>
+    </div>
+  );
+};
+
+export default Events;
